4ba32798608ccc8cfc7f3e4c6fd174101c5c99c5
Khinshan Khan <khanshan501@gmail.com>
2018-10-13 11:43:07 -0400

    proper build script

--- _flask/build.sh
+++ _flask/build.sh
@@ -1,10 +1,6 @@
 cd ~/Documents/site
 
-rm -rf !(.|..|.git|_flask|README.md|CNAME)
+rm -rf !(.|..|.git|.gitignore|_flask|README.md|CNAME)
 python _flask/app.py build
 mv _flask/build/* .
-mv frozenflasktest/* .
-rm -rf _flask/build frozenflasktest/
-
-mv about about.html
-mv tags tags.html
+rm -rf _flask/build
