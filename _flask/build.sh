5b9af36ab28d0bf25422b04e6d49924c283256f4
Khinshan Khan <khanshan501@gmail.com>
2019-01-13 21:21:45 -0500

    FIXED: build script got messed up somehow

--- _flask/build.sh
+++ _flask/build.sh
@@ -1,6 +1,13 @@
-cd ~/Documents/site
+SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
+cd $SCRIPTPATH
 
+cd ..
 rm -rf !(.|..|.git|.gitignore|_flask|README.md|CNAME)
-python _flask/app.py build
-mv _flask/build/* .
-rm -rf _flask/build
+
+cd _flask
+python app.py build
+emacs --script autoindent.el build
+mv build/* ..
+rm -rf build
+
+cd ..
