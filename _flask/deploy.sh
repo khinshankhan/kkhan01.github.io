5e2854f8782ccc8983ed5bdf81c6d577bce9e251
Khinshan Khan <khanshan501@gmail.com>
2019-04-06 19:56:03 -0400

    hmm this may be a clean enough script

--- _flask/deploy.sh
+++ _flask/deploy.sh
@@ -5,9 +5,11 @@ cd ..
 rm -rf !(.|..|.git|.gitignore|_flask|README.md|CNAME)
 
 cd _flask
+emacs --script org2md.el build
 python3 app.py build
 emacs --script autoindent.el build
 mv build/* ..
 rm -rf build
+rm -rf content/posts/*md* content/posts/*~
 
 cd ..
